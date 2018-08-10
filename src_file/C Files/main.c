f763b272fd4ce63169006775ce7161b08cb73918
Janrey Licas <janjan.licas@gmail.com>
2018-08-10 23:01:46 +0800

    Headstarted C File

--- src_file/C Files/main.c
+++ src_file/C Files/main.c
@@ -1,18 +1,23 @@
+// Group LM - Enrollment System in C for Preliminary Project in Programming
 #include <stdio.h>
-#include <conio.h>
-#include <windows.h> // Windows Compatibility, Portability RIP
+#include <stdlib.h> // For SYSTEM Command
+#include <conio.h> // For Getch()
+#include <windows.h> // Windows Designed Only Header, This Rips Compatibility.
 #include <winsock2.h> // Added
+#include <time.h> // For Time Display and Such
+// defined marcros to avoid typing these words again. 
+//Calling those will result into the following strings
+#define VERSION_NUMBER "2259-08102018"
+#define PRODUCT_NAME "Group LM | Enrollment System for TiP Engineering and Architecture"
 // Include Expected Header Such as ICMP for Communication in Windows
-// Paste
-
-int main() {
+int stdnt_Number(int Btch_Year, int Btch_Number, int Btch_Identity_Number);
+int POST_Logon(char Pass, char User);
+// INCLUDE ALL FUNCTION PROTOTYPE HERE, BECAUSE FUNCTIONS DONE WITH BODY IS 
+//GOING TO BE PLACE BELOW FROM INT MAIN FUNCTION
+int main(int argc, char *argv[]) {
 // Init POST
 // Choose a Type Login
 // User and Pass
 // Create a Database that checks for login through Excel or Mongo DB or any API DB if possible.
 
-
-
-getch(); // Because why not?
-    return 0;
 }
