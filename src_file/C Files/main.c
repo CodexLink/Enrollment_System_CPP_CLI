bef4ad0f09a1c4d0a154b03aeec60c9518ed701f
ci-mascarenas <42256689+ci-mascarenas@users.noreply.github.com>
2018-08-10 10:01:58 +0800

    Updated Prototype

--- src_file/C Files/main.c
+++ src_file/C Files/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h> // Windows Compatibility, Portability RIP
-
+#include <winsock2.h> // Added
+// Include Expected Header Such as ICMP for Communication in Windows
 
 
 int main() {
+// Init POST
+// Choose a Type Login
+// User and Pass
+// Create a Database that checks for login through Excel or Mongo DB or any API DB if possible.
 
 
 
-
-
+getch(); // Because why not?
     return 0;
 }
