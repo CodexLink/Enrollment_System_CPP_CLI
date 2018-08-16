f58b6ffd7ed73bc222d44e53731e9f8c1b883f85
Janrey Licas <janjan.licas@gmail.com>
2018-08-16 23:39:37 +0800

    Prototyping Experimental Argc and Argv Accepter Done

--- Experimental/Experimental_Argc_Argv_Extended.c
+++ Experimental/Experimental_Argc_Argv_Extended.c
@@ -4,15 +4,18 @@
 #include <windows.h> // Windows Designed Only Header, This Rips Compatibility.
 #include <string.h>  // String Handling for Usage of Structure and Database
 #define VERSION_NUMBER "0853-08142018-BETA"
-#define PRODUCT_NAME                                                           \
-  "Group LM | C CLI Enrollment System for TiP Engineering and Architecture"
+#define PRODUCT_NAME "Group LM | C CLI Enrollment System for TiP Engineering and Architecture"
 #define ERROR_1 "WARN.UnableReposition"
 #define ERROR_2 "WARN.Win32Disable_Denied"
 #define WARN_1 "ERROR.UnableToConnect"
 #define WARN_2 "ERROR.UnableToCheckStorage"
 #define ACMPLT "Critical Components Checkpoint Completed"
 
-char Argument_Initialization(argc, argv);
+char Argument_Initialization(int argc, char *argv[]);
+int Function(Argument_1_Bool, Argument_2_Bool);
+int Function_Start_1();
+int Function_Start_2();
+
 int main(int argc, char *argv[]) {
   int a;
   char ArgumentModeReturn;
@@ -29,27 +32,34 @@ int main(int argc, char *argv[]) {
 
 char Argument_Initialization(int argc, char *argv[]) {
   int CheckerFinalArray;
-  int Argument_Checker;
+  int Argument_Checker, Argument_Checker_1, Argument_1_Bool, Argument_2_Bool;
   printf("\n @ Function | char Argument_Initialization(int argc, char *argv[])\n");
-  // Create a Guard Checker for Exceeding Arguments
 
-  /*if (Argument_DefaultAvailable[2] != argv[2]) {
-              printf("Counter Arguments Exceeded the Required One");
-      }*/
-  // Create a Per Parameter All Size of Argument_DefaultAvailable will be
-  // checked.
-  // DO NOT Use Size!, Get Literal Function To Check for Same String.
-  // NOT YET DONE.
-  // Use if and dont compare anymore. It will be more harder than I thought
-  for (Argument_Checker = 1; Argument_Checker < argc; Argument_Checker++) {
-      if (argv[Argument_Checker] = "-fullscreen") {
-        printf("Initializing Mode %s\n", argv[Argument_Checker]);
-     }
-      if (argv[Argument_Checker] = "-management") {
-        printf("Initializing Mode %s\n", argv[Argument_Checker]);      
+  while(Argument_Checker != argc) {
+      if (strcmp(argv[Argument_Checker], "-fullscreen") == 0 ) {
+        Argument_1_Bool = 1;
       }
+      if (strcmp(argv[Argument_Checker], "-management") == 0 ) {
+        Argument_2_Bool = 1;
+      }
+      Argument_Checker++;
+  }
+  Function(Argument_1_Bool, Argument_2_Bool);
+}
+int Function (int Argument_1_Bool, int Argument_2_Bool) {
+    if (Argument_1_Bool == 1) {
+      Function_Start_1();
     }
-    //printf("%s == %s\n", Argument_DefaultAvailable[Argument_Checker],argv[Argument_Checker + 1]);
-    //CheckerFinalArray = strcmp(Argument_DefaultAvailable[Argument_Checker],argv[Argument_Checker]);
-
+    if (Argument_2_Bool == 1) {
+      Function_Start_2();
+    }
+    printf("Press any key to continue...");
+    getch();
+    return EXIT_SUCCESS;
+  }
+int Function_Start_1() {
+  printf("Success Start @ Function_Start_1\n");
+}
+int Function_Start_2() {
+  printf("Success Start @ Function_Start_2\n");
 }
